b450250f75f8eb7ecea34e5afc0750b72f8edf74
Siddharth Telang <siddharth.telang910@gmail.com>
2022-08-28 17:33:34 -0400

    Show initial unfiltered matches

--- SfM.py
+++ SfM.py
@@ -1,5 +1,7 @@
 from Utils.DataLoader import extractMatchesFromFile
-from Utils.ImageUtils import readImageSet
+from Utils.ImageUtils import readImageSet, showMatches
+import numpy as np
+import os
 
 folder_name = r"C:\Users\siddh\Documents\733\SfM\Data"
 save_path = r"C:\Users\siddh\Documents\733\SfM\outputs"
@@ -9,3 +11,11 @@ total_images = len(images)
 feature_x, feature_y, \
     feature_flag, feature_descriptor = \
         extractMatchesFromFile(folder_name, total_images)
+
+for i in range(0, total_images-1):
+    for j in range(i+1, total_images):
+        idx = np.where(feature_flag[:, i] & feature_flag[:, j])
+        pts1 = np.hstack((feature_x[idx, i].reshape((-1, 1)), feature_y[idx, i].reshape((-1, 1))))
+        pts2 = np.hstack((feature_x[idx, j].reshape((-1, 1)), feature_y[idx, j].reshape((-1, 1))))
+        showMatches(images[i], images[j], pts1, pts2,
+                   fileName=os.path.join(save_path, (str(i)+str(j)+".png")))
--- Utils/ImageUtils.py
+++ Utils/ImageUtils.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 def readImageSet(folder_name):
     print("Reading images from ", folder_name)
@@ -11,3 +12,35 @@ def readImageSet(folder_name):
                 images.append(cv2.imread(img_path))
                 print(f"Loading image {file}")
     return images
+
+def makeImageSizeSame(images):
+    sizes = []
+    for image in images:
+        x, y, ch = image.shape
+        sizes.append([x, y, ch])
+
+    sizes = np.array(sizes)
+    x_target, y_target, _ = np.max(sizes, axis = 0)
+    
+    images_resized_list = []
+
+    for i, image in enumerate(images):
+        image_resized = np.zeros((x_target, y_target, sizes[i, 2]), np.uint8)
+        image_resized[0:sizes[i, 0], 0:sizes[i, 1], 0:sizes[i, 2]] = image
+        images_resized_list.append(image_resized)
+    return images_resized_list
+
+def showMatches(image1, image2, pts1, pts2, color=(0,0,255), fileName=""):
+    image_1, image_2 = makeImageSizeSame([image1, image2])
+    concatenated = np.concatenate((image_1, image_2), axis=1)
+    if pts1 is not None:
+        pts1_x = pts1[:, 0].astype(int)
+        pts1_y = pts1[:, 1].astype(int)
+        pts2_x = pts2[:, 0].astype(int) + image_1.shape[1]
+        pts2_y = pts2[:, 1].astype(int)
+        for i in range(pts1_x.shape[0]):
+            cv2.line(concatenated, (pts1_x[i], pts1_y[i]), (pts2_x[i], pts2_y[i]), color, 1)
+        cv2.imshow(fileName, concatenated)
+        cv2.imwrite(fileName, concatenated)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
