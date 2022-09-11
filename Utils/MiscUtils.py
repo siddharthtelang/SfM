756071fae506764d3063ed6adaadc1d5746a9cae
Siddharth Telang <siddharth.telang910@gmail.com>
2022-09-11 12:12:17 -0400

    Add Utils for Rotation, Quaternion, Euler and Homogenous coordinates

--- Utils/MiscUtils.py
+++ Utils/MiscUtils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.transform import Rotation 
 
 def ProjectionMatrix(R, C, K):
     C = np.reshape(C, (3, 1))        
@@ -26,5 +27,23 @@ def ReprojectionError(X, pt1, pt2, R1, C1, R2, C2, K):
     e1 = np.sum(np.square(np.linalg.norm(pt1-p1, axis=0)))
     e2 = np.sum(np.square(np.linalg.norm(pt2-p2, axis=0)))
 
-    return e1+e2
+    return (e1+e2)/X.shape[0]
 
+def getQuaternion(R2):
+    Q = Rotation.from_matrix(R2)
+    return Q.as_quat()
+
+def getEuler(R2):
+    euler = Rotation.from_matrix(R2)
+    return euler.as_rotvec()
+
+def getRotation(Q, type_ = 'q'):
+    if type_ == 'q':
+        R = Rotation.from_quat(Q)
+        return R.as_matrix()
+    elif type_ == 'e':
+        R = Rotation.from_rotvec(Q)
+        return R.as_matrix()
+
+def makeHomogenous(pts):
+    return np.hstack((pts, np.ones((pts.shape[0], 1))))
